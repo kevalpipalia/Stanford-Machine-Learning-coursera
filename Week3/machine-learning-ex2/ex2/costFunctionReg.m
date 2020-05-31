f59da6f63e074f3881d644665c143967d40b3c4b
Keval Pipalia <keval.pipalia009@yahoo.com>
2020-05-31 10:36:36 +0530

    Cmnpleted all assigments till week 6
    All assignments and lecture notes till week 6

--- Week3/machine-learning-ex2/ex2/costFunctionReg.m
+++ Week3/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,7 +17,10 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+J = 1 / m * sum( -y .* log(sigmoid(X*theta)) - (1-y) .* log(1- sigmoid(X*theta))) + lambda / 2 / m * sum(theta(2:end).^2);
 
+grad(1) = 1 / m * X(:,1)' * (sigmoid(X*theta) - y);
+grad(2:end) = 1 / m * X(:,2:end)' * (sigmoid(X*theta) - y) + lambda / m * theta(2:end);
 
 
 
--- Week3/machine-learning-ex2/ex2/predict.m
+++ Week3/machine-learning-ex2/ex2/predict.m
@@ -15,6 +15,9 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+p = sigmoid(X*theta);
+p(p>=0.5) = 1;
+p(p<0.5) = 0;
 
 
 
